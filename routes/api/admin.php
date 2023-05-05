4c1b202ca5c12d3d36300181f7c0f3fe8b636558
ArcosPerez <49565559+ArcosPerez@users.noreply.github.com>
2023-05-04 22:27:48 -0600

    Crud event

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -49,9 +49,9 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::group(['prefix' => 'event'], function () {
         Route::controller(EventController::class)->group(function () {
             Route::post('/index', 'index');
-            Route::patch('/{team}', 'update');
+            Route::post('/{event}', 'update');
             Route::post('', 'store');
-            Route::delete('/{team}', 'destroy');
+            Route::delete('/{event}', 'destroy');
         });
 
 
